d21ec397cc8f14f446310dfc0065d1123099f8a8
Anders Bakken <agbakken@gmail.com>
2022-04-25 11:42:57 -0700

    Better printing.

--- model/Printer.js
+++ model/Printer.js
@@ -14,7 +14,7 @@ class Printer
     {
         let byStack = this.model.pageFaultsByStack.get(stack);
         if (byStack) {
-            this.model.printPageFaults(stack, byStack);
+            this.printPageFaults(stack, byStack);
         } else {
             console.log("No page faults with stack", stack, "\n", Array.from(this.model.pageFaultsByStack.keys()));
         }
@@ -24,7 +24,7 @@ class Printer
     {
         let byStack = this.model.pageFaultsByMmapStack.get(stack);
         if (byStack) {
-            this.model.printPageFaults(stack, byStack);
+            this.printPageFaults(stack, byStack);
         } else {
             console.log("No page faults with mmap stack", stack);
         }
@@ -46,7 +46,7 @@ class Printer
         if (pageFaults.length === 1) {
             console.log(`The page fault happened at ${prettyMS(first)} in "${this.model.data.strings[threads[0]]}"`);
         } else {
-            console.log(`The page faults happened between ${prettyMS(first)} and ${last}ms in these threads: ${threads.map(x => "\"" + this.model.data.strings[x] + "\"")}`);
+            console.log(`The page faults happened between ${prettyMS(first)} and ${last}ms in these thread(s): ${threads.map(x => "\"" + this.model.data.strings[x] + "\"")}`);
         }
         console.log(Stack.print(this.model.data.stacks[stack], this.model.data.strings));
     }
@@ -72,12 +72,12 @@ class Printer
         for (const [key, value] of this.model.pageFaultsByStack) {
             sorted.push({ stack: key, length: value.reduce((cur, x, idx) => cur + x.range.length, 0) });
         }
-        sorted.sort((a, b) => a.length - b.length).forEach((x, idx) => {
-            if (idx < 10) {
-                this.model.printPageFaultsAtStack(x.stack);
-            }
+        sorted = sorted.sort((a, b) => b.length - a.length);
+        sorted.length = Math.min(10, sorted.length);
+        sorted.forEach((x, idx) => {
+            console.log(`---------------- ${idx} ----------------`);
+            this.printPageFaultsAtStack(x.stack);
         });
-
     }
 };
 
