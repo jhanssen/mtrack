9beabce3a264e4ed849f7aa166cc0dc054872417
Jan Erik Hanssen <jhanssen@gmail.com>
2022-05-05 13:16:29 -0700

    improve mmap sample

--- samples/mmap/MmapSample.cpp
+++ samples/mmap/MmapSample.cpp
@@ -1,27 +1,90 @@
+#include <Preload.h>
 #include <sys/mman.h>
-#include <stdio.h>
+#include <cstdio>
+#include <cstdint>
+// brings in std::size
+#include <string>
+#include <variant>
 
 #define PAGESIZE 4096
-#define NUMPAGES 4
+
+struct Command {
+    enum class Type { Mmap, Munmap, Madvise, Touch, Snapshot };
+
+    Type type {};
+    intptr_t arg1 {};
+    intptr_t arg2 {};
+    intptr_t arg3 {};
+    intptr_t arg4 {};
+
+    uintptr_t ret {};
+};
+
+static void runCommands(Command* cmds, size_t numCommands)
+{
+    for (size_t c = 0; c < numCommands; ++c) {
+        Command& cmd = cmds[c];
+        switch (cmd.type) {
+        case Command::Type::Mmap:
+            cmd.ret = reinterpret_cast<uintptr_t>(::mmap(nullptr, cmd.arg1, cmd.arg2, cmd.arg3, -1, 0));
+            break;
+        case Command::Type::Munmap: {
+            uint8_t* ptr = nullptr;
+            if (cmd.arg1 >= 0) {
+                // relative to start
+                ptr = reinterpret_cast<uint8_t*>(cmds[cmd.arg1].ret);
+            } else {
+                // relative to current
+                ptr = reinterpret_cast<uint8_t*>(cmds[c + cmd.arg1].ret);
+            }
+            cmd.ret = ::munmap(ptr + cmd.arg2, cmd.arg3);
+            break; }
+        case Command::Type::Madvise: {
+            uint8_t* ptr = nullptr;
+            if (cmd.arg1 >= 0) {
+                // relative to start
+                ptr = reinterpret_cast<uint8_t*>(cmds[cmd.arg1].ret);
+            } else {
+                // relative to current
+                ptr = reinterpret_cast<uint8_t*>(cmds[c + cmd.arg1].ret);
+            }
+            cmd.ret = ::madvise(ptr + cmd.arg2, cmd.arg3, cmd.arg4);
+            break; }
+        case Command::Type::Touch: {
+            char* ptr = nullptr;
+            if (cmd.arg1 >= 0) {
+                // relative to start
+                ptr = reinterpret_cast<char*>(cmds[cmd.arg1].ret);
+            } else {
+                // relative to current
+                ptr = reinterpret_cast<char*>(cmds[c + cmd.arg1].ret);
+            }
+            // touch n (arg3) pages from arg1 + arg2
+            ptr += cmd.arg2;
+            for (size_t n = 0; n < cmd.arg3; ++n) {
+                *(ptr + (n * PAGESIZE) + 1) = 'a';
+            }
+            break; }
+        case Command::Type::Snapshot:
+            mtrack_snapshot();
+            break;
+        }
+    }
+}
 
 int main(int, char**)
 {
-    printf("mmapping\n");
-    void* ptr = mmap(nullptr, PAGESIZE * NUMPAGES, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    if (!ptr) {
-        printf("no mmap\n");
-        return 0;
-    }
+    mtrack_disable_snapshots();
 
-    // touch the pages
-    char* c = reinterpret_cast<char*>(ptr);
-    for (int i = 0; i < NUMPAGES; ++i) {
-        if (i % 2)
-            continue;
-        printf("touch page %d\n", i);
-        *(c + (i * PAGESIZE) + 1) = 'a';
-    }
-    printf("sample done\n");
-    munmap(ptr, PAGESIZE * NUMPAGES);
+    Command cmds[] = {
+        { Command::Type::Mmap, 100 * PAGESIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS },
+        { Command::Type::Touch, -1, 0, 100 },
+        { Command::Type::Snapshot },
+        { Command::Type::Munmap, -3, 10 * PAGESIZE, 50 * PAGESIZE },
+        { Command::Type::Snapshot }
+    };
+
+    runCommands(cmds, std::size(cmds));
+
     return 0;
 }
